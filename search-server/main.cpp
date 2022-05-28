4cc52ff3853068f3475c88dc9e272fd0d6f70bfa
Vyacheslav Semenov <vyasem@gmail.com>
2022-05-28 21:20:13 +0300

    throw

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -8,6 +8,9 @@
 #include <cmath>
 #include <tuple>
 #include <numeric>
+#include <cassert>
+#include <optional>
+#include <stdexcept>
 
 unsigned MAX_RESULT_DOCUMENT_COUNT = 5;
 const double EPSILON = 1e-6;
@@ -30,19 +33,44 @@ struct Document {
 
 class SearchServer{
 public:
+	inline static constexpr int INVALID_DOCUMENT_ID = -1;
 	SearchServer(const std::string& stopWords){
 		 for (const std::string& word : SplitIntoWords(stopWords)) {
-			stop_words.insert(word);
+			 for(const char ch: word){
+				int code = int(ch);
+				if(code >= 0 && code < 32){
+					throw std::invalid_argument(" stop word contains a wrong character");
+				}
+			 }
+			 stop_words.insert(word);
 		}
 	}
 
 	SearchServer(const std::vector<std::string>& stopWords){
 		for (const std::string& word : stopWords) {
+			for(const char ch: word){
+				int code = int(ch);
+				if(code >= 0 && code < 32){
+					throw std::invalid_argument(" stop word contains a wrong character");
+				}
+			 }
 			stop_words.insert(word);
 		}
 	}
-	SearchServer(const std::set<std::string>& stopWords): stop_words(stopWords){}
+	SearchServer(const std::set<std::string>& stopWords): stop_words(stopWords){
+		for(const std::string& word: stopWords){
+			for(const char ch: word){
+				int code = int(ch);
+				if(code >= 0 && code < 32){
+					throw std::invalid_argument(" stop word contains a wrong character");
+				}
+			 }
+		 }
+	}
 	void AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& docRating) {
+		if(!checkDocument(document, document_id)){
+			throw std::invalid_argument(" invalid add document");
+		}
 	    const std::vector<std::string> words = SplitIntoWordsNoStop(document, stop_words);
 	    int size = words.size();
         double tf = 1.0 / size;
@@ -57,6 +85,9 @@ public:
 
 	template <typename Predicat>
 	std::vector<Document> FindTopDocuments(const std::string& raw_query, Predicat filter)const {
+		if(!checkQuery(raw_query)){
+			throw std::invalid_argument(" a query string is invalid");
+		}
 		 const std::set<std::string> query_words = ParseQuery(raw_query, stop_words);
 		 std::vector<Document> allDoc = FindAllDocuments(query_words, filter);
 		 sort(allDoc.begin(), allDoc.end(), [](const Document& lhs, const Document& rhs) {
@@ -72,7 +103,7 @@ public:
 	}
 
 	std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentStatus status)const {
-		 return FindTopDocuments(raw_query, [status](int document_id, DocumentStatus documentStatus, int rating){
+		return FindTopDocuments(raw_query, [status](int document_id, DocumentStatus documentStatus, int rating){
 			 return documentStatus == status;
 		 });
 	}
@@ -81,15 +112,20 @@ public:
 		 return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
 	}
 
-	std::tuple<	std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query, int document_id) const{
+	std::tuple<std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query, int document_id) const{
+		if(!checkQuery(raw_query)){
+			throw std::invalid_argument(" a query string is invalid");
+		}
 		std::set<std::string> query_words = ParseQuery(raw_query, stop_words);
 		std::vector<std::string> findWords;
 		DocumentStatus status = documentStatus.at(document_id);
+		std::tuple<std::vector<std::string>, DocumentStatus> result;
 		for(const std::string& word: query_words){
 			if(word[0] == '-'){
 				std::string newWord = word.substr(1);
 				if(documents.count(newWord) && documents.at(newWord).count(document_id)){
-					return {findWords, status};
+					result = {findWords, status};
+					return result;
 				}
 			}
 
@@ -97,19 +133,57 @@ public:
 				findWords.push_back(word);
 			}
 		}
-
-		return {findWords, status};
+		result = {findWords, status};
+		return result;
 	}
 
-	int GetDocumentCount(){
+	int GetDocumentCount() const{
 		return documentsCount;
 	}
+
+	int GetDocumentId(int index) const {
+		if(index < 0 || index >= documentsCount){
+			throw std::out_of_range("out of range");
+		}
+		return index;
+	}
+
 private:
 	int documentsCount = 0;
 	std::map<std::string, std::map<int, double>> documents;
 	std::set<std::string> stop_words;
 	std::map<int, int> documentsRating;
 	std::map<int, DocumentStatus> documentStatus;
+
+	bool checkQuery(const std::string& query)const{
+		unsigned size = query.size();
+		for(unsigned i = 0; i < size; ++i){
+			int code = int(query[i]);
+			if((query[i] == '-' && (i < (size - 1) && query[i+1] == '-')) || (code >= 0 && code < 32)){
+				return false;
+			}
+
+			if(query[i] == '-' && (i == size - 1 ||  query[i+1] == ' ')){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool checkDocument(const std::string& document, int document_id)const{
+		unsigned size = document.size();
+		if(documentStatus.count(document_id)){
+			return false;
+		}
+		for(unsigned i = 0; i < size; ++i){
+			int code = int(document[i]);
+			if((code >= 0 && code < 32) || document_id < 0){
+				return false;
+			}
+
+		}
+		return true;
+	}
 
 	static int ComputeAverageRating(const std::vector<int>& ratings) {
 		return std::accumulate(ratings.begin(), ratings.end(), 0) / static_cast<int>(ratings.size());
@@ -141,7 +215,6 @@ private:
 	    if (!word.empty()) {
 	        words.push_back(word);
 	    }
-
 	    return words;
 	}
 
@@ -208,257 +281,62 @@ void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& w
 	std::cout << "}" << std::endl;
 }
 
-// -----------установка макросов ---------------
-template<typename T>
-void runTest(T f, const std::string& functionName){
-	f();
-	std::cerr << functionName << " OK" << std::endl;
+void AddDocument(SearchServer& search_server, int document_id, const std::string& document, DocumentStatus status,
+                 const std::vector<int>& ratings) {
+    try {
+        search_server.AddDocument(document_id, document, status, ratings);
+    } catch (const std::exception& e) {
+        std::cout << "Ошибка добавления документа "s << document_id << ": "s << e.what() << std::endl;
+    }
 }
 
-#define RUN_TEST(func) runTest(func, #func)
-
-template<typename T, typename U>
-void assertEqual(const T& lhs,const U& rhs, const std::string& lhsText, const std::string& rhsText, const std::string& fileName, const int lineNumber, const std::string& functionName){
-	if(lhs != rhs){
-		std::cerr << std::boolalpha;
-		std::cerr << fileName << "("s << lineNumber << "): "s << functionName << ": ASSERT(" << lhsText << " != " << rhsText << ") failed." << std::endl;
-		abort();
-	}
-}
-template<typename T, typename U>
-void assertEqualHint(const T& lhs,const U& rhs, const std::string& lhsText, const std::string& rhsText, const std::string& fileName, const int lineNumber, const std::string& functionName, const std::string& hint){
-	if(lhs != rhs){
-		std::cerr << std::boolalpha;
-		std::cerr << fileName << "("s << lineNumber << "): "s << functionName << ": ASSERT(" << lhsText << " != " << rhsText << ") failed. Hint: " << hint << std::endl;
-		abort();
-	}
+void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query) {
+	std::cout << "Результаты поиска по запросу: "s << raw_query << std::endl;
+    try {
+        for (const Document& document : search_server.FindTopDocuments(raw_query)) {
+            PrintDocument(document);
+        }
+    } catch (const std::exception& e) {
+    	std::cout << "Ошибка поиска: "s << e.what() << std::endl;
+    }
 }
 
-#define ASSERT_EQUAL(lhs, rhs) assertEqual((lhs), (rhs), #lhs, #rhs, __FILE__, __LINE__, __FUNCTION__)
-#define ASSERT_EQUAL_HINT(lhs, rhs, hint) assertEqualHint((lhs), (rhs), #lhs, #rhs, __FILE__, __LINE__, __FUNCTION__, (hint))
-
-void userAssert(bool expr, const std::string& text, const std::string& fileName, const int lineNumber, const std::string& functionName){
-	if(!expr){
-		std::cerr << std::boolalpha;
-		std::cerr << fileName << "("s << lineNumber << "): "s << functionName << ": ASSERT(" << text << ") failed." << std::endl;
-		abort();
-	}
+void MatchDocuments(const SearchServer& search_server, const std::string& query) {
+    try {
+    	std::cout << "Матчинг документов по запросу: "s << query << std::endl;
+        const int document_count = search_server.GetDocumentCount();
+        for (int index = 0; index < document_count; ++index) {
+            const int document_id = search_server.GetDocumentId(index);
+            const auto [words, status] = search_server.MatchDocument(query, document_id);
+            PrintMatchDocumentResult(document_id, words, status);
+        }
+    } catch (const std::exception& e) {
+    	std::cout << "Ошибка матчинга документов на запрос "s << query << ": "s << e.what() << std::endl;
+    }
 }
 
-void userAssertHint(bool expr, const std::string& text, const std::string& fileName, const int lineNumber, const std::string& functionName, const std::string& hint){
-	if(!expr){
-		std::cerr << std::boolalpha;
-		std::cerr << fileName << "("s << lineNumber << "): "s << functionName << ": ASSERT(" << text << ") failed. Hint: " << hint << std::endl;
-		abort();
-	}
-}
-
-#define ASSERT(expr) userAssert((expr), #expr, __FILE__, __LINE__, __FUNCTION__)
-#define ASSERT_HINT(expr, hint) userAssertHint((expr), #expr, __FILE__, __LINE__, __FUNCTION__, (hint))
-// -----------установка макросов ---------------
-// -------- Начало модульных тестов поисковой системы ----------
-
-
-SearchServer genereateServerObject(){
-	const std::string stopWords = "greater why not near without sure most had mr still never greatest be she"s;
-	SearchServer server(stopWords);
-	server.AddDocument(0, "highly respect inquietude finished had greater none speaking", DocumentStatus::ACTUAL, {1, 5, 8});
-	server.AddDocument(1, "having regret round kept remainder myself why not weather wished he made taste soon assistance eyes near", DocumentStatus::ACTUAL, {2, 3, 9});
-	server.AddDocument(3, "without inquietude invited never ladies relation reasonable secure humoured", DocumentStatus::ACTUAL, {1, 2});
-	server.AddDocument(4, "smiling sure furnished purse had most offered adapted called correct does domestic", DocumentStatus::BANNED, {5});
-	server.AddDocument(5, "excellence mr still alteration depending never seven first greatest three park", DocumentStatus::REMOVED, {4, 5, 7, 9});
-	server.AddDocument(6, "suspicion be miles bed sure continue instantly sentiments rejoiced laughing rapid she", DocumentStatus::IRRELEVANT, {5});
-	return server;
-}
-
-
-void TestConstrucorStopWord(){
-	const std::string stopWords = "greater why not near without sure most had mr still never greatest be she"s;
-	SearchServer server(stopWords);
-	std::vector<std::string> stopWordsV = {"и"s, "в"s, "на"s};
-	SearchServer search_server(stopWords);
-}
-
-
-void TestExcludeStopWordsFromAddedDocumentContent() {
-	SearchServer server = genereateServerObject();
-	const auto found_docs = server.FindTopDocuments("humoured"s);
-	ASSERT_EQUAL(static_cast<int>(found_docs.size()), 1);
-	const Document& doc0 = found_docs[0];
-	ASSERT_EQUAL_HINT(doc0.id, 3, "Wrong ID");
-	ASSERT_HINT(server.FindTopDocuments("without"s).empty(), "Must be empty");
-	ASSERT(!server.FindTopDocuments("weather"s).empty());
+void test(){
 
 }
-
-void TestExcludeMinusWord(){
-	SearchServer server = genereateServerObject();
-	ASSERT(server.FindTopDocuments("-highly speaking"s).empty());
-	ASSERT_HINT(!server.FindTopDocuments("excellence"s, DocumentStatus::REMOVED).empty(),  "Wrong!");
-}
-
-void TestMatchDocument(){
-	SearchServer server = genereateServerObject();
-	std::vector<std::string> words;
-	DocumentStatus status;
-	std::tie(words, status) = server.MatchDocument( "car regret round"s, 1);
-	ASSERT_EQUAL(static_cast<int>(words.size()), 2);
-	std::tie(words, status) = server.MatchDocument( "root invited -relation"s, 3);
-	ASSERT(words.empty());
-	std::tie(words, status) = server.MatchDocument( "-root invited"s, 3);
-	ASSERT_EQUAL_HINT(static_cast<int>(words.size()), 1, "It must be 1");
-	std::tie(words, status) = server.MatchDocument( ""s, 0);
-	ASSERT(words.empty());
-	std::tie(words, status) = server.MatchDocument( "root -having -regret"s, 1);
-	ASSERT(words.empty());
-}
-
-void TestRelevanceSort(){
-	SearchServer server = genereateServerObject();
-	const std::vector<Document> result = server.FindTopDocuments("invited inquietude weather made assistance finished"s);
-	ASSERT_EQUAL(result[0].id, 0);
-	ASSERT_EQUAL(result[1].id, 3);
-	ASSERT_EQUAL_HINT(result[2].id, 1, "It's bad value");
-}
-
-
-void TestRatingCalculate(){
-	SearchServer server = genereateServerObject();
-	const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure"s);
-	ASSERT_EQUAL(result[0].rating, 4);
-	ASSERT_EQUAL(result[1].rating, 1);
-	ASSERT_EQUAL(result[2].rating, 4);
-}
-
-void TestStatusSearch(){
-	SearchServer server = genereateServerObject();
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure"s);
-		ASSERT_EQUAL(static_cast<int>(result.size()), 3);
-		ASSERT_EQUAL(result[0].id, 0);
-		ASSERT_EQUAL(result[1].id, 3);
-		ASSERT_EQUAL(result[2].id, 1);
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, DocumentStatus::IRRELEVANT);
-		ASSERT_EQUAL(static_cast<int>(result.size()), 1);
-		ASSERT_EQUAL(result[0].id, 6);
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, DocumentStatus::BANNED);
-		ASSERT_EQUAL(static_cast<int>(result.size()), 1);
-		ASSERT_EQUAL(result[0].id, 4);
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, DocumentStatus::REMOVED);
-		ASSERT_EQUAL(static_cast<int>(result.size()), 1);
-		ASSERT_EQUAL(result[0].id, 5);
-	}
-}
-
-void TestRelevanceValue(){
-	SearchServer server = genereateServerObject();
-	const std::vector<Document> result = server.FindTopDocuments("invited inquietude weather made assistance finished"s);
-	ASSERT(std::abs(result[0].relevance - 0.481729) < 1e-6);
-	ASSERT(std::abs(result[1].relevance - 0.41291) < 1e-6);
-	ASSERT_HINT(std::abs(result[2].relevance - 0.383948) < 1e-6, "Left value must be less that 1e-6");
-}
-
-void TestPredicatUse(){
-	SearchServer server = genereateServerObject();
-	{
-		const std::vector<Document> result = server.FindTopDocuments("invited inquietude weather made assistance finished"s, [](int document_id, DocumentStatus documentStatus, int rating){
-			 return rating == 4;
-		 });
-		ASSERT_EQUAL(static_cast<int>(result.size()), 2);
-		ASSERT_EQUAL(result[0].id, 0);
-		ASSERT_EQUAL(result[1].id, 1);
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, [](int document_id, DocumentStatus documentStatus, int rating){
-			 return documentStatus == DocumentStatus::BANNED;
-		 });
-		ASSERT_EQUAL(result[0].id, 4);
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, [](int document_id, DocumentStatus documentStatus, int rating){
-			 return document_id == 6;
-		 });
-		ASSERT_EQUAL(result[0].rating, 5);
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, [](int document_id, DocumentStatus documentStatus, int rating){
-			 return document_id == 59;
-		 });
-		ASSERT(result.empty());
-	}
-
-	{
-		const std::vector<Document> result = server.FindTopDocuments("highly regret invited purse alteration sure suspicion"s, [](int document_id, DocumentStatus documentStatus, int rating){
-			 return rating == 5;
-		 });
-		ASSERT_EQUAL(static_cast<int>(result.size()), 2);
-		ASSERT_EQUAL(result[0].id, 4);
-		ASSERT_EQUAL(result[1].id, 6);
-	}
-
-}
-
-void TestDocumentCount(){
-	SearchServer server = genereateServerObject();
-	int count = server.GetDocumentCount();
-	ASSERT_EQUAL(count, 6);
-}
-
-
-void TestSearchServer() {
-	RUN_TEST(TestConstrucorStopWord);
-	RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
-	RUN_TEST(TestExcludeMinusWord);
-	RUN_TEST(TestMatchDocument);
-	RUN_TEST(TestRelevanceSort);
-	RUN_TEST(TestRatingCalculate);
-	RUN_TEST(TestStatusSearch);
-	RUN_TEST(TestRelevanceValue);
-	RUN_TEST(TestPredicatUse);
-	RUN_TEST(TestDocumentCount);
-}
-
-// --------- Окончание модульных тестов поисковой системы -----------
-
 
 
 
 int main() {
-	TestSearchServer();
-    std::vector<std::string> stopWords = {"и"s, "в"s, "на"s};
-    SearchServer search_server(stopWords);
-    search_server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
-    search_server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::ACTUAL, {7, 2, 7});
-    search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
-    search_server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
+    SearchServer search_server("и в на"s);
 
-    std::cout << "ACTUAL by default:"s << std::endl;
-    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s)) {
-        PrintDocument(document);
-    }
+    AddDocument(search_server, 1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    AddDocument(search_server, 1, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2});
+    AddDocument(search_server, -1, "пушистый пёс и модный ошейник"s, DocumentStatus::ACTUAL, {1, 2});
+    AddDocument(search_server, 3, "большой пёс скво\x12рец евгений"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    AddDocument(search_server, 4, "большой пёс скворец евгений"s, DocumentStatus::ACTUAL, {1, 1, 1});
 
-    std::cout << "BANNED:"s << std::endl;
-    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, DocumentStatus::BANNED)) {
-        PrintDocument(document);
-    }
+    FindTopDocuments(search_server, "пушистый -пёс"s);
+    FindTopDocuments(search_server, "пушистый --кот"s);
+    FindTopDocuments(search_server, "пушистый -"s);
 
-    std::cout << "Even ids:"s << std::endl;
-    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
-        PrintDocument(document);
-    }
-
-    return 0;
+    MatchDocuments(search_server, "пушистый пёс"s);
+    MatchDocuments(search_server, "модный -кот"s);
+    MatchDocuments(search_server, "модный --пёс"s);
+    MatchDocuments(search_server, "пушистый - хвост"s);
 }
 
